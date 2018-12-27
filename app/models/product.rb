b98bc047bd0dadc15f91aecd3491a29b62118e60
Joe <josephd.carlson@gmail.com>
2018-12-27 06:16:33 -0500

    Rails 5 junk

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :quote
+  belongs_to :quote, optional: true
   has_many :product_details
 
   accepts_nested_attributes_for :product_details, :allow_destroy => true
